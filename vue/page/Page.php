b7372c008a179a6dac43eda40b4dec50abb7e7d6
Celeste Tollec <celeste.tollec@gmail.com>
2025-11-14 14:26:13 +0100

    Implementation de Page pour le rendue et contenue de test dans PageAcceuil

--- vue/page/Page.php
+++ vue/page/Page.php
@@ -2,6 +2,27 @@
 namespace vue\page;
 
 abstract class Page {
+    protected $title = "Page";
+    protected $content;
     
+    public function __construct($title = "Page") {
+        $this->title = $title;
+    }
+
+    protected function renderHeader() {
+        require_once __DIR__ . '/../composant/Header.php';
+    }
+
+    protected function renderFooter() {
+        require_once __DIR__ . '/../composant/Footer.php';
+    }
+ 
+    abstract protected function renderContent();
+
+    public function render() {
+        $this->renderHeader();
+        $this->renderContent();
+        $this->renderFooter();
+    }
 }
 ?>
--- vue/page/PageAccueil.php
+++ vue/page/PageAccueil.php
@@ -1,7 +1,20 @@
 <?php
 namespace vue\page;
 
+require_once __DIR__ . '/Page.php';
+
 class PageAccueil extends Page {
-    
+    protected function renderContent() {
+        ?>
+        <section class="content">
+            <h2>Bienvenue</h2>
+            <p>Contenu de votre page d'accueil</p>
+        </section>
+        <?php
+    }
 }
+
+// Utilisation
+$page = new PageAccueil("Accueil");
+$page->render();
 ?>
