3e016aa31c8e5a049e91e5524e0204975bd6641c
Rafaël HOFF <hoff.rafael06@gmail.com>
2025-11-20 23:12:45 +0100

    Ajout d'un moyen de connection à la bdd (marche pas forcément mdr) + ajout de ConstanteDAO

---
+++ service/ConnectionBDD.php
@@ -0,0 +1,40 @@
+<?php
+namespace service;
+
+use PDO;
+use PDOException; 
+
+/**
+ * Classe de service pour établir la connexion à la base de données.
+ */
+class ConnectionBDD {
+    
+    /**
+     * Établit et retourne la connexion PDO.
+     * @return PDO L'objet de connexion à la base de données.
+     */
+    public static function connect(): PDO {
+        try {
+            $dsn = 'mysql:host=localhost;dbname=bdd_celeste;charset=utf8;port=3307';
+            $username = 'root';
+            $password = ''; 
+
+            $db = new PDO(
+                $dsn, 
+                $username, 
+                $password
+            );
+
+            // Configuration des options PDO pour une meilleure gestion des erreurs
+            // Ceci permet à PDO de lancer des exceptions en cas d'erreur SQL, au lieu de retourner false.
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            // Retourne l'objet de connexion
+            return $db;
+
+        } catch (PDOException $e) {
+            die('Erreur de connexion à la BDD : ' . $e->getMessage());
+        }
+    }
+}
+?>
