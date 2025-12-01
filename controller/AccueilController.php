6de6611d2dd4e2e15c99246bf8667d3e3764686f
Celeste Tollec <celeste.tollec@gmail.com>
2025-12-01 10:09:20 +0100

    Avancement du changement de page d'aceuil en fonction de PhaseVote

--- controller/AccueilController.php
+++ controller/AccueilController.php
@@ -2,11 +2,19 @@
 namespace controller;
 
 require_once __DIR__ . '/../vue\page\PageAccueil.php';
+require_once __DIR__ . '/../service/SessionManager.php';
+require_once __DIR__ . '/../service/Enum.php';
+
 use vue\page\PageAccueil;
+use service\SessionManager;
+use service\PhaseVote;
 
 class AccueilController {
     public function index() {
-        $page = new PageAccueil("Accueil");
+        $session = SessionManager::getInstance();
+        $phase = $session->getPhaseVote() ?? 'preVote';
+
+        $page = new PageAccueil("Accueil", $phase);
         $page->render(); // le contrôleur déclenche l’affichage
     }
 }
--- vue/page/PageAccueil.php
+++ vue/page/PageAccueil.php
@@ -2,15 +2,51 @@
 namespace vue\page;
 
 require_once __DIR__ . '/Page.php';
+require_once __DIR__ . '/../../service/Enum.php';
+
+use service\PhaseVote;
 
 class PageAccueil extends Page {
+    private PhaseVote $phase;
+
+    public function __construct($title, $phase) {
+        parent::__construct($title);
+        $this->phase = $phase;
+    }
+
     protected function renderContent() {
         ?>
         <section class="content">
-            <h2>Bienvenue</h2>
-            <p>Contenu de votre page</p>
+        <?php
+        echo "<h1>" . htmlspecialchars($this->phas) . "</h1>";
+        switch ($this->phase) {
+            case PhaseVote::PreVote:
+                echo "<h2>Bienvenue sur Toptracks</h2>";
+                echo "<p>Le prochaint vote commencera le ...</p>";
+                echo '<a href="?page=inscription">Créer un compte pour etre pres à voter</a>';
+                break;
+            case PhaseVote::Vote1:
+                echo "<h2>Phase Vote 1</h2>";
+                $this->AfficherCategories();
+                break;
+            case PhaseVote::Vote2:
+                echo "<h2>Phase Vote 2</h2>";
+                $this->AfficherCategories();
+                break;
+            case PhaseVote::Resultats:
+                echo "<h2>Résultats</h2>";
+                $this->AfficherCategories();
+                break;
+            default:
+                echo "<p>Phase inconnue.</p>";
+        }
+        ?>
         </section>
         <?php
+    }
+
+    private function AfficherCategories() {
+        echo "<h3>Catégories de vote :</h3>";
     }
 }
 
