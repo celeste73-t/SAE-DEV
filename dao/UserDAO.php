1556373627087a9f10594a8951519d52efe8e26b
Celeste_Tollec <celeste73310@gmail.com>
2025-12-12 13:59:10 +0100

    reparation des injection de dépendance dans les DAO

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../model/User.php';
 require_once __DIR__ . '/../service/Enum.php';
 require_once __DIR__ . '/../service/ConnectionBDD.php';
 
+use service\ConnectionBDD;
 use PDO;
 use PDOException;
 use model\User;
@@ -16,9 +17,9 @@ class UserDAO {
      * Initialise le DAO avec l'objet de connexion PDO.
      * @param PDO $db L'objet de connexion à la base de données.
      */
-    public function __construct(PDO $db) {
+    public function __construct() {
         // Injection de la dépendance PDO
-        $this->db = $db;
+        $this->db = ConnectionBDD::connect();
     }
 
     /**
