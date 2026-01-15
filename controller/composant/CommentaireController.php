5e7eea84dad0a41bf5e74124bc96063bf540f857
Celeste TOLEC <celeste73310@gmail.com>
2026-01-15 16:18:31 +0100

    Reponse au commentaire

--- controller/composant/CommentaireController.php
+++ controller/composant/CommentaireController.php
@@ -12,18 +12,33 @@ use vue\composant\CommentaireView;
 class CommentaireController {
 
     public function build(Commentaire $commentaire, string $auteur) {
-        return new CommentaireView($commentaire, $auteur);
+        $commentaireDAO = new CommentaireDAO(); 
+        $commentairesData = $commentaireDAO->getReponses($commentaire->getId());
+        
+        $commentairesViews = []; 
+        foreach ($commentairesData as $row) { 
+            $postController = new CommentaireController();
+            $commentairesViews[] =  $postController->build($row['commentaire'],   $row['auteur']);
+        }
+        return new CommentaireView($commentaire, $auteur, $commentairesViews);
     }
 
     public function create() {
         $contenu = $_POST['contenu'] ?? ''; 
-        $userId = $_POST['userId'] ?? '';
-        $postId = $_POST['postId'] ?? '';
+        $userId = $_POST['userId'];
+        $postId = $_POST['postId'] ?? null;
+        $commentaireId = $_POST['commentaireId'] ?? null;
 
         $commentaire = new Commentaire(null, $contenu);
 
         $commentaireDAO = new CommentaireDAO(); 
-        $commentaireDAO->createCommentaire($commentaire, $userId, $postId, null); 
+        if ($postId && !$commentaireId) { 
+            $commentaireDAO->createCommentaire($commentaire, $userId, $postId, null); 
+        } 
+        
+        if ($commentaireId) { 
+            $commentaireDAO->createCommentaire($commentaire, $userId, null, $commentaireId); 
+        }
         header("Location: index.php?page=validation"); 
         exit;
     }
--- vue/composant/CommentaireView.php
+++ vue/composant/CommentaireView.php
@@ -9,24 +9,38 @@ use model\Commentaire;
 use service\SessionManager;
 
 class CommentaireView extends Composant{
-    private $commentaire;
-    private $auteur;
+    private Commentaire $commentaire;
+    private string $auteur;
+    private array $commentaires;
 
-    public function __construct(Commentaire $commentaire, string $auteur){
+    public function __construct(Commentaire $commentaire, string $auteur, array $commentaires){
         $this->commentaire = $commentaire;
         $this->auteur = $auteur;
+        $this->commentaires = $commentaires;
         parent::__construct("commentaire");
     }
     
     protected function renderContent() {
         $session = SessionManager::getInstance()
         ?>
-        <div class="post-header">
+        <div class="commentaire-header">
             <strong><?php echo $this->auteur ?></strong>
         </div>
 
         <p><?php echo $this->commentaire->getContenu() ?></p>
+        <form action="index.php?page=commentaire&action=create" method="POST" class="form-commentaire">
+            <input type="hidden" name="commentaireId" value="<?= $this->commentaire->getId() ?>">
+            <input type="hidden" name="userId" value="<?= 
+            $session->getUser()->getId() ?>">
+
+            <label>Comment</label>
+            <input type="text" name="contenu" required>
+
+            <button type="submit">Publier</button>
+        </form>
         <?php 
-        
+        foreach ($this->commentaires as $commentaire) {
+            $commentaire->render();
+        }
     }
 }
